03e3632f6cae2d83d9b376782c55137179947c38
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-02 19:37:58 -0300

    test: cria produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    ID = 1
+    NOME_DO_PRODUTO = 'produto'
+    NOME_DA_EMPRESA = 'empresa'
+    DATA_DE_FAB = '10/02/2022'
+    DATA_DE_VAL = '20/12/2022'
+    NUMERO_DE_SERIE = 252145
+    INSTRUCOES_DE_ARMAZENAMENTO = 'Local seco e longe do sol'
+
+    product = Product(
+        ID,
+        NOME_DO_PRODUTO,
+        NOME_DA_EMPRESA,
+        DATA_DE_FAB, DATA_DE_VAL,
+        NUMERO_DE_SERIE,
+        INSTRUCOES_DE_ARMAZENAMENTO
+    )
+
+    assert product.id == ID
+    assert product.nome_do_produto == NOME_DO_PRODUTO
+    assert product.nome_da_empresa == NOME_DA_EMPRESA
+    assert product.data_de_fabricacao == DATA_DE_FAB
+    assert product.data_de_validade == DATA_DE_VAL
+    assert product.numero_de_serie == NUMERO_DE_SERIE
+    assert product.instrucoes_de_armazenamento == INSTRUCOES_DE_ARMAZENAMENTO
