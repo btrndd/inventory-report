939e48d6d74b4d62a35f932a7d8d9b4c7585fbfd
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-03 19:51:55 -0300

    feat: main func

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,35 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        sys.stderr.write("Verifique os argumentos\n")
+        return
+
+    args = sys.argv
+    file_path = args[1]
+    file_format = file_path[-4:]
+    report_type = args[2]
+
+    if (file_format == '.csv'):
+        inventory = InventoryRefactor(CsvImporter)
+        report = inventory.import_data(file_path, report_type)
+        sys.stdout.write(report)
+        return
+    elif(file_format == 'json'):
+        inventory = InventoryRefactor(JsonImporter)
+        report = inventory.import_data(file_path, report_type)
+        sys.stdout.write(report)
+        return
+    elif(file_format == '.xml'):
+        inventory = InventoryRefactor(XmlImporter)
+        report = inventory.import_data(file_path, report_type)
+        sys.stdout.write(report)
+        return
+    else:
+        sys.stderr.write("Arquivo inválido")
+        return
